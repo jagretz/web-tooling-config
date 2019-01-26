487aa800b7f13549db461f3adb37a262506635cc
jason gretz <jagretz@gmail.com>
2019-01-25 20:02:16 -0500

    Testing out enquier with async/await

--- packages/web-configs-cli/cli.js
+++ packages/web-configs-cli/cli.js
@@ -1,9 +1,11 @@
 const fs = require("fs");
 const path = require("path");
-// @see https://nodejs.org/dist/latest-v8.x/docs/api/util.html#util_util_promisify_original
+/** @see [promisify]{@link https://nodejs.org/dist/latest-v8.x/docs/api/util.html#util_util_promisify_original} */
 const { promisify } = require("util");
-// https://nodejs.org/api/child_process.html#child_process_child_process
+/** @see [child_process]{@link https://nodejs.org/api/child_process.html#child_process_child_process} */
 const { exec, spawn } = require("child_process");
+/** @see [enquirer]{@link https://www.npmjs.com/package/enquirer} */
+const { prompt } = require("enquirer");
 /* custom modules / non-node modules */
 const logger = require("./colorLog");
 
@@ -24,7 +26,7 @@ const execCmd = promisify(exec);
 async function asyncStat() {
     // let stats;
     try {
-        return await statPromise(path.join(cwd, '.git'));
+        return await statPromise(path.join(cwd, ".git"));
         // console.log(`Success! ${stats}`);
     } catch (error) {
         if (error.code === "ENOENT" && error.path.endsWith(".git")) {
@@ -35,21 +37,55 @@ async function asyncStat() {
             path: 'C:\\ngss\\g\\web-configs\\packages\\web-configs-cli\\.git'
             */
             // console.error(`meow > Git not detected. Make sure you run this command from a "clean" git directory.`);
-            logger.error(`Git not detected. Make sure you run this command from within a "clean" git directory.`);
+            logger.error(
+                `Git not detected. Make sure to run this command from within a "clean" git directory.`
+            );
         } else {
             console.error("Problem detected:\n", error);
         }
         return null;
     }
-
 }
 
-asyncStat()
+const questions = [
+    {
+        type: "input",
+        name: "FILENAME",
+        message: "What is the name of the file without extension?"
+    },
+    {
+        type: "list",
+        name: "EXTENSION",
+        message: "What is the file extension?",
+        initial: "bork",
+        choices: [".rb", ".js", ".php", ".css"],
+        filter: function(val) {
+            return val.split(".")[1];
+        }
+    }
+];
+
+async function run() {
+    asyncStat();
+    welcomeMessage();
+    const response = ask(questions);
+    console.log("response :", response);
+}
+
+async function ask(questions) {
+    return await prompt(questions);
+}
+
+run();
+
+function welcomeMessage() {
+    console.log(logger.log("Let's configure that app 😉"));
+}
 
 // todo
 /*
-- start the cli...
-- Ensure a clean git directory. This will be used as a "backup".
+- [x] start the cli...
+- [x] Ensure a clean git directory. This will be used as a "backup".
 - read in files from dev dependencies
     - try just reading in a single file for now
     - ignore non-essential deps
@@ -60,4 +96,3 @@ Other
 - handle errors
 - prompt user before install
 */
-
