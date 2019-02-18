a2352f3c8bfe14fc5b7bb36334fe0ba75a296f1f
jason gretz <jagretz@gmail.com>
2019-02-18 07:07:38 -0500

    Upd eslint configs

--- packages/eslint-config-react/eslint-react.js
+++ packages/eslint-config-react/eslint-react.js
@@ -2,7 +2,7 @@ module.exports = {
     extends: ["airbnb/rules/react", "airbnb/rules/react-a11y"],
     rules: {
         "react/jsx-indent": ["error", 4],
-        "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
+        "react/jsx-filename-extension": ["error", { extensions: [".js"] }],
         "react/jsx-one-expression-per-line": 0,
         "jsx-a11y/label-has-for": [
             2,
