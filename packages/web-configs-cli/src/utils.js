fdb85cef2c154998de19700a366daa5634e7f4ca
jagretz <jagretz@gmail.com>
2019-03-10 16:32:09 -0400

    Created utility to install package.devDependencies
    - Uses ChildProcess.spawn wrapped in a promise to allow the use of async/await -- similar to how other async utilis are handled.

--- packages/web-configs-cli/src/utils.js
+++ packages/web-configs-cli/src/utils.js
@@ -12,11 +12,59 @@ async function filterPackageDependencies(projectDependencies, packageJson) {
     // perf is not a big deal here considering how few items we are
     // looping through, however, performance improvements can be future feature 👍
     return projectDependencies.reduce((accum, curr) => {
-        console.log("accum", accum);
+        if (Reflect.has(packageJson, curr)) {
+            return accum;
+        }
+
+        accum.push(curr);
         return accum;
     }, []);
 }
 
+/*
+Why we use `child_process.spawn` over `child_process.exec`
+
+exec buffers the command's entire output instead of using a stream. In other
+words, you'll receive the WHOLE output instead of a stream of messages
+as each message (output) is generated.
+
+spawn can directly use IO streams of the parent/caller by specifying
+`stdio: inherit` option. This allows the output from the executing script to
+be displayed as messages are emitted (received).
+*/
+/* where code = { success: 0, error: 1 }  */
+async function safeSpawn(spawnNpmProcess) {
+    try {
+        await spawnAsPromise(spawnNpmProcess);
+        return 0;
+    } catch (error) {
+        console.log("Error occured in safeSpawn:", error);
+        return 1;
+    }
+}
+
+function spawnAsPromise(invokeProcess) {
+    /*
+    ! `error` from `spawn` is not the same as `stderr`.
+    Errors are throwable and can be caught with a try/catch. `stderr` are
+    streams written as received and therefore not throwable unless collected
+    through a buffer and thrown as an `Error`.
+    */
+    return new Promise((resolve, reject) => {
+        const sp = invokeProcess();
+
+        /* code: { success: 0, error: 1 }  */
+        sp.on("close", code => {
+            if (code === 0) {
+                resolve();
+            }
+            reject();
+        });
+        process.on("error", reject);
+    });
+}
+
 module.exports = {
-    filterPackageDependencies
+    filterPackageDependencies,
+    safeSpawn
 };
